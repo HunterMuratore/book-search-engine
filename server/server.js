65b44fe1e7654d3c7df9429c2dc5df63eb28d92f
HunterMuratore <muratoreh@gmail.com>
2023-12-05 18:37:43 -0500

    startServer function

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, expressMiddleware } = require('apollo-server-express');
+
 const path = require('path');
 const db = require('./config/connection');
+
 const { typeDefs, resolvers } = require('./schema');
 const { authMiddleware } = require('./utils/auth');
 
@@ -11,20 +13,26 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware, // Use context to pass authentication information if needed
+  context: authMiddleware,
 });
 
-// Apply the ApolloServer middleware to Express
-server.applyMiddleware({ app });
+async function startServer() {
+  await server.start();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  // Apply the ApolloServer middleware to Express
+  server.applyMiddleware({ app });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
+  });
 }
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
-});
+startServer();
